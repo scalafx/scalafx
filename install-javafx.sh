2f96d5254a3f9c71e2a75bf9e065e5a6cde7c47c
Sven Reimers <sreimers@netbeans.org>
2011-09-22 01:01:54 +0200

    Small script to help install javafx binaries into local maven repo. Usage: ./install-javafx.sh /path/to/javafx/home os.name

---
+++ install-javafx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export JFX_HOME=$1
+export OS=$2
+mvn install:install-file -Dfile=$JFX_HOME/rt/lib/jfxrt.jar -DgroupId=com.oracle -DartifactId=javafx-runtime -Dversion=2.0-beta -Dpackaging=jar
+cd $JFX_HOME/rt/bin
+jar -cf bin.jar *.*lib
+mvn install:install-file -Dfile=bin.jar -DgroupId=com.oracle -DartifactId=javafx-runtime -Dversion=2.0-beta -Dpackaging=jar -Dclassifier=$2
