f788d1122e8e070140a0f9ce4164ad6ce0230972
Stephen Chin <steveonjava@gmail.com>
2012-03-10 10:15:03 -0800

    Fixed permission issue with modifying javafx installation folder under build user.

--- install-javafx.sh
+++ install-javafx.sh
@@ -2,6 +2,5 @@
 export JFX_HOME=$1
 export OS=$2
 mvn install:install-file -Dfile=$JFX_HOME/rt/lib/jfxrt.jar -DgroupId=com.oracle -DartifactId=javafx-runtime -Dversion=2.0-beta -Dpackaging=jar
-cd $JFX_HOME/rt/lib
-jar -cf lib.jar *.*lib i386
+jar -cf lib.jar $JFX_HOME/rt/lib/*.*lib $JFX_HOME/rt/lib/i386
 mvn install:install-file -Dfile=lib.jar -DgroupId=com.oracle -DartifactId=javafx-runtime -Dversion=2.0-beta -Dpackaging=jar -Dclassifier=$2
