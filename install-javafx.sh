53b89dc598f702cc8d605352778e1933ac4b37d9
Stephen Chin <steveonjava@gmail.com>
2012-03-10 10:12:25 -0800

    Fixed install script to work with new packaging in JavaFX 2.1

--- install-javafx.sh
+++ install-javafx.sh
@@ -2,6 +2,6 @@
 export JFX_HOME=$1
 export OS=$2
 mvn install:install-file -Dfile=$JFX_HOME/rt/lib/jfxrt.jar -DgroupId=com.oracle -DartifactId=javafx-runtime -Dversion=2.0-beta -Dpackaging=jar
-cd $JFX_HOME/rt/bin
-jar -cf bin.jar *.*lib i386
-mvn install:install-file -Dfile=bin.jar -DgroupId=com.oracle -DartifactId=javafx-runtime -Dversion=2.0-beta -Dpackaging=jar -Dclassifier=$2
+cd $JFX_HOME/rt/lib
+jar -cf lib.jar *.*lib i386
+mvn install:install-file -Dfile=lib.jar -DgroupId=com.oracle -DartifactId=javafx-runtime -Dversion=2.0-beta -Dpackaging=jar -Dclassifier=$2
