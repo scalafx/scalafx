f187ca9a146140070fb9e5b463024ddc7b5e018b
Stephen Chin <steveonjava@gmail.com>
2012-03-10 09:40:43 -0800

    Build fix for Linux-based platforms.

--- install-javafx.sh
+++ install-javafx.sh
@@ -3,5 +3,5 @@ export JFX_HOME=$1
 export OS=$2
 mvn install:install-file -Dfile=$JFX_HOME/rt/lib/jfxrt.jar -DgroupId=com.oracle -DartifactId=javafx-runtime -Dversion=2.0-beta -Dpackaging=jar
 cd $JFX_HOME/rt/bin
-jar -cf bin.jar *.*lib
+jar -cf bin.jar *.*lib i386
 mvn install:install-file -Dfile=bin.jar -DgroupId=com.oracle -DartifactId=javafx-runtime -Dversion=2.0-beta -Dpackaging=jar -Dclassifier=$2
