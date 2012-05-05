781916b6e5df5574ef81020444b6d92c02202288
Tani <Tani@compu>
2012-05-05 12:01:40 -0300

    Added scalafx.geometry.Side scalafx.scene.chart.Chart scalafx.scene.chart.PieChart

---
+++ src/scalafx/geometry/Side.scala
@@ -0,0 +1,56 @@
+
+
+/*
+ * Copyright (c) 2011, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package scalafx.geometry
+
+import javafx.{geometry => jfxg}
+import scalafx.util.SFXDelegate
+
+//I think it might be better to implemented this way rather than extending Enumeration
+object Side {
+  implicit def sfxSide2jfx(c: Side) = c.delegate
+  
+  val BOTTOM = new Side(jfxg.Side.BOTTOM)
+  val LEFT = new Side(jfxg.Side.LEFT)
+  val RIGHT = new Side(jfxg.Side.RIGHT)
+  val TOP = new Side(jfxg.Side.TOP)
+  
+  def valueOf(name: String) = name match {
+    case "BOTTOM" => BOTTOM
+    case "LEFT" => LEFT
+    case "RIGHT" => RIGHT
+    case "TOP" => TOP
+  }
+  def values = List(BOTTOM, LEFT, RIGHT, TOP)
+}
+
+class Side(override val delegate:jfxg.Side) extends SFXDelegate[jfxg.Side] {
+  def isHorizontal = delegate.isHorizontal()
+  def isVertical = delegate.isVertical()
+}
---
+++ src/scalafx/scene/chart/PieChart.scala
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2011, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package scalafx.scene.chart
+
+import javafx.scene.{chart => jfxsc}
+import scalafx.Includes._
+import scalafx.collections.ObservableBuffer
+import scalafx.util.SFXDelegate
+
+object PieChart {
+  implicit def sfxPieChart2jfx(v: PieChart) = v.delegate
+  
+  object Data {
+    implicit def sfxPieChartData2jfx(v: PieChart.Data) = v.delegate
+    
+    def apply(name: String, value: Double) = new PieChart.Data(new jfxsc.PieChart.Data(name, value))
+  }
+  
+  class Data(override val delegate:jfxsc.PieChart.Data) extends SFXDelegate[jfxsc.PieChart.Data] {
+    def chart = delegate.chartProperty
+    
+    def name = delegate.nameProperty
+    def name_= (v: String) {
+      name() = v
+    }
+    
+    def pieValue = delegate.pieValueProperty
+    def pieValue_= (v: Double) {
+      pieValue() = v
+    }
+    
+    def node = delegate.getNode
+  }
+  
+  def apply(data: ObservableBuffer[jfxsc.PieChart.Data]) = new PieChart(new jfxsc.PieChart(data))
+}
+
+class PieChart(override val delegate:jfxsc.PieChart = new jfxsc.PieChart()) extends Chart(delegate) with SFXDelegate[jfxsc.PieChart] {
+  def clockwise = delegate.clockwiseProperty
+  def clockwise_= (v: Boolean) {
+    clockwise() = v
+  }
+  
+  def data = delegate.dataProperty
+  def data_= (v: ObservableBuffer[jfxsc.PieChart.Data]) {
+    data() = v
+  }
+
+  def labelLineLength = delegate.labelLineLengthProperty
+  def labelLineLength_= (v: Double) {
+    labelLineLength() = v
+  }
+  
+  def labelsVisible = delegate.labelsVisibleProperty
+  def labelsVisible_= (v: Boolean) {
+    labelsVisible() = v
+  }
+  
+  def startAngle = delegate.startAngleProperty
+  def startAngle_= (v: Double) {
+    startAngle() = v
+  }
+  
+}
