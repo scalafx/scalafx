775d03e53137d97ee596e848fe81210cceb727a5
Tani <Tani@compu>
2012-05-06 11:07:22 -0300

    Added scalafx.scene.chart.Axis scalafx.scene.chart.XYChart

---
+++ src/scalafx/scene/chart/Axis.scala
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2011, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package scalafx.scene.chart
+
+import javafx.scene.{chart => jfxsc}
+
+import scala.collection.JavaConversions._
+import scala.collection.mutable.Buffer
+
+import scalafx.Includes._
+import scalafx.beans.binding._
+import scalafx.collections.ObservableBuffer
+import scalafx.geometry.Side
+import scalafx.scene.paint.Paint
+import scalafx.scene.layout.Region
+import scalafx.scene.text.Font
+import scalafx.util.SFXDelegate
+
+object Axis {
+  implicit def sfxAxis2jfx[T](v: Axis[T]) = v.delegate
+  
+  object TickMark {
+    implicit def sfxTickMark2jfx[T](v: Axis.TickMark[T]) = v.delegate
+  }
+  
+  final class TickMark[T](override val delegate: jfxsc.Axis.TickMark[T] = new jfxsc.Axis.TickMark[T]()) extends SFXDelegate[jfxsc.Axis.TickMark[T]] {
+    def label = delegate.labelProperty
+    def label_= (value: String) = delegate.setLabel(value)
+    
+    def position = delegate.positionProperty
+    def position_= (value: Double) = delegate.setPosition(value)
+    
+    def value = delegate.valueProperty
+    def value_= (value: T) = delegate.setValue(value)
+    
+    def textVisible = delegate.isTextVisible
+    def textVisible_= (v: Boolean) = delegate.setTextVisible(v)
+  }
+}
+
+abstract class Axis[T](override val delegate: jfxsc.Axis[T]) extends Region(delegate) with SFXDelegate[jfxsc.Axis[T]] {
+  def animated = delegate.animatedProperty
+  def animated_= (v: Boolean) {
+    animated() = v
+  }
+  
+  def autoRanging = delegate.autoRangingProperty
+  def autoRanging_= (v: Boolean) {
+    autoRanging() = v
+  }
+  
+  def label = delegate.labelProperty
+  def label_= (v: String) {
+    label() = v
+  }
+  
+  def side = delegate.sideProperty
+  def side_= (v: Side) {
+    side() = v
+  }
+  
+  def tickLabelFill = delegate.tickLabelFillProperty
+  def tickLabelFill_= (v: Paint) {
+    tickLabelFill() = v
+  }
+  
+  def tickLabelFont = delegate.tickLabelFontProperty
+  def tickLabelFont_= (v: Font) {
+    tickLabelFont() = v
+  }
+  
+  def tickLabelGap = delegate.tickLabelGapProperty
+  def tickLabelGap_= (v: Double) {
+    tickLabelGap() = v
+  }
+  
+  def tickLabelRotation = delegate.tickLabelRotationProperty
+  def tickLabelRotation_= (v: Double) {
+    tickLabelRotation() = v
+  }
+  
+  def tickLabelsVisible = delegate.tickLabelsVisibleProperty
+  def tickLabelsVisible_= (v: Boolean) {
+    tickLabelsVisible() = v
+  }
+  
+  def tickLength = delegate.tickLengthProperty
+  def tickLength_= (v: Double) {
+    tickLength() = v
+  }
+  
+  def tickMarkVisible = delegate.tickMarkVisibleProperty
+  def tickMarkVisible_= (v: Boolean) {
+    tickMarkVisible() = v
+  }
+  
+  def displayPosition(value: T) = delegate.getDisplayPosition(value)
+  
+  def tickMarks: ObservableBuffer[jfxsc.Axis.TickMark[T]] = delegate.getTickMarks
+  
+  def valueForDisplay(displayPosition: Double): T = delegate.getValueForDisplay(displayPosition)
+  
+  def zeroPosition = delegate.getZeroPosition
+  
+  def invalidateRange(data: Buffer[T]) = delegate.invalidateRange(data)
+  
+  def isValueOnAxis(value: T) = delegate.isValueOnAxis(value)
+  
+  def requestAxisLayout = delegate.requestAxisLayout
+  
+  def requestLayout = delegate.requestLayout
+  
+  def toNumericValue(value: T) = delegate.toNumericValue(value)
+  
+  def toRealValue(value: Double): T = delegate.toRealValue(value)
+  
+}
---
+++ src/scalafx/scene/chart/XYChart.scala
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2011, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package scalafx.scene.chart
+
+import javafx.scene.{chart => jfxsc}
+import scalafx.Includes._
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.Node
+import scalafx.util.SFXDelegate
+
+object XYChart {
+  implicit def sfxXYChart2jfx[X, Y](v: XYChart[X, Y]) = v.delegate
+  
+  object Data {
+    implicit def sfxXYChartData2jfx[X, Y](v: XYChart.Data[X, Y]) = v.delegate
+    
+    def apply[X, Y](x: X, y: Y) = new XYChart.Data[X, Y](new jfxsc.XYChart.Data[X, Y](x, y))
+    
+    def apply[X, Y](x: X, y: Y, extraValue: AnyRef) = new XYChart.Data[X, Y](new jfxsc.XYChart.Data[X, Y](x, y, extraValue))
+  }
+  
+  class Data[X, Y](override val delegate: jfxsc.XYChart.Data[X, Y] = new jfxsc.XYChart.Data[X, Y]()) extends SFXDelegate[jfxsc.XYChart.Data[X, Y]] {
+    def extraValue = delegate.extraValueProperty
+    def extraValue_= (v: AnyRef) {
+      extraValue() = v
+    }
+    
+    def node = delegate.nodeProperty
+    def node_= (v: Node) {
+      node() = v
+    }
+    
+    def XValue = delegate.XValueProperty
+    def XValue_= (v: X) = delegate.setXValue(v)
+    
+    def YValue = delegate.YValueProperty
+    def YValue_= (v: Y) = delegate.setYValue(v)
+  }
+  
+  object Series {
+    implicit def sfxXYChartSeries2jfx[X, Y](v: XYChart.Series[X, Y]) = v.delegate
+    
+    def apply[X, Y](data: ObservableBuffer[jfxsc.XYChart.Data[X, Y]]) = new XYChart.Series[X, Y](new jfxsc.XYChart.Series[X, Y](data))
+    
+    def apply[X, Y](name: String, data: ObservableBuffer[jfxsc.XYChart.Data[X, Y]]) = new XYChart.Series[X, Y](new jfxsc.XYChart.Series[X, Y](name, data))
+  }
+  
+  class Series[X, Y](override val delegate: jfxsc.XYChart.Series[X, Y] = new jfxsc.XYChart.Series[X, Y]()) extends SFXDelegate[jfxsc.XYChart.Series[X, Y]] {
+    def chart = delegate.chartProperty
+    
+    def data = delegate.dataProperty
+    def data_= (v: ObservableBuffer[jfxsc.XYChart.Data[X, Y]]) {
+      data() = v
+    }
+    
+    def name = delegate.nameProperty
+    def name_= (v: String) {
+      name() = v
+    }
+    
+    def node = delegate.nodeProperty
+    def node_= (v: Node) {
+      node() = v
+    }
+  }
+  
+}
+
+abstract class XYChart[X, Y](override val delegate:jfxsc.XYChart[X, Y]) extends Chart(delegate) with SFXDelegate[jfxsc.XYChart[X, Y]] {
+  def alternativeColumnFillVisible = delegate.alternativeColumnFillVisibleProperty
+  def alternativeColumnFillVisible_= (v: Boolean) {
+    alternativeColumnFillVisible() = v
+  }
+  
+  def alternativeRowFillVisible = delegate.alternativeRowFillVisibleProperty
+  def alternativeRowFillVisible_= (v: Boolean) {
+    alternativeRowFillVisible() = v
+  }
+  
+  def data = delegate.dataProperty
+  def data_= (v: ObservableBuffer[jfxsc.XYChart.Series[X, Y]]) {
+    data() = v
+  }
+  
+  def horizontalGridLinesVisible = delegate.horizontalGridLinesVisibleProperty
+  def horizontalGridLinesVisible_= (v: Boolean) {
+    horizontalGridLinesVisible() = v
+  }
+  
+  def horizontalZeroLineVisible = delegate.horizontalZeroLineVisibleProperty
+  def horizontalZeroLineVisible_= (v: Boolean) {
+    horizontalZeroLineVisible() = v
+  }
+  
+  def verticalGridLinesVisible = delegate.verticalGridLinesVisibleProperty
+  def verticalGridLinesVisible_= (v: Boolean) {
+    verticalGridLinesVisible() = v
+  }
+  
+  def verticalZeroLineVisible = delegate.verticalZeroLineVisibleProperty
+  def verticalZeroLineVisible_= (v: Boolean) {
+    verticalZeroLineVisible() = v
+  }
+  
+}
