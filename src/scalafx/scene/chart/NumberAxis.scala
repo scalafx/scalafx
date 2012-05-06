6e9936ab3be3133a7f027f093534326bc6599884
Tani <Tani@compu>
2012-05-06 20:28:20 -0300

    Added scalafx.scene.chart.AreaChart scalafx.scene.chart.CategoryAxis scalafx.scene.chart.NumberAxis scalafx.scene.chart.ValueAxis

---
+++ src/scalafx/scene/chart/NumberAxis.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2011, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package scalafx.scene.chart
+
+import javafx.scene.{chart => jfxsc}
+import scalafx.Includes._
+import scalafx.collections.ObservableBuffer
+import scalafx.util.SFXDelegate
+import scalafx.util.converter.StringConverterDelegate
+
+object NumberAxis {
+  implicit def sfxNumberAxis2jfx(v: NumberAxis) = v.delegate
+  
+  def apply(lowerBound: Double, upperBound: Double, tickUnit: Double) = new NumberAxis(new jfxsc.NumberAxis(lowerBound, upperBound, tickUnit))
+  
+  def apply(axisLabel: String, lowerBound: Double, upperBound: Double, tickUnit: Double) = new NumberAxis(new jfxsc.NumberAxis(axisLabel, lowerBound, upperBound, tickUnit))
+  
+  object DefaultFormatter {
+    implicit def sfxDefaultFormatter2jfx(v: DefaultFormatter) = v.delegate
+    
+    def apply(axis: NumberAxis) = new DefaultFormatter(new jfxsc.NumberAxis.DefaultFormatter(axis))
+    
+    def apply(axis: NumberAxis, prefix: String, suffix: String) = new DefaultFormatter(new jfxsc.NumberAxis.DefaultFormatter(axis, prefix, suffix))
+  }
+  
+  class DefaultFormatter(override val delegate:jfxsc.NumberAxis.DefaultFormatter) extends StringConverterDelegate[java.lang.Number, Number, jfxsc.NumberAxis.DefaultFormatter](delegate)
+}
+
+final class NumberAxis(override val delegate:jfxsc.NumberAxis = new jfxsc.NumberAxis) extends ValueAxis[Number](delegate) with SFXDelegate[jfxsc.NumberAxis] {
+  def forceZeroInRange = delegate.forceZeroInRangeProperty
+  def forceZeroInRange_= (v: Boolean) {
+    forceZeroInRange() = v
+  }
+  
+  def tickUnit = delegate.tickUnitProperty
+  def tickUnit_= (v: Double) {
+    tickUnit() = v
+  }
+  
+}
---
+++ src/scalafx/scene/chart/ValueAxis.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2011, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package scalafx.scene.chart
+
+import javafx.scene.{chart => jfxsc}
+import scalafx.Includes._
+import scalafx.collections.ObservableBuffer
+import scalafx.util.SFXDelegate
+import scalafx.util.StringConverter
+
+object ValueAxis {
+  implicit def sfxValueAxis2jfx[T <: Number](v: ValueAxis[T]) = v.delegate
+}
+
+abstract class ValueAxis[T <: Number](override val delegate:jfxsc.ValueAxis[T]) extends Axis[T](delegate) with SFXDelegate[jfxsc.ValueAxis[T]] {
+  def lowerBound = delegate.lowerBoundProperty
+  def lowerBound_= (v: Double) {
+    lowerBound() = v
+  }
+
+  def minorTickCount = delegate.minorTickCountProperty
+  def minorTickCount_= (v: Int) {
+    minorTickCount() = v
+  }
+  
+  def minorTickLength = delegate.minorTickLengthProperty
+  def minorTickLength_= (v: Double) {
+    minorTickLength() = v
+  }
+
+  def minorTickVisible = delegate.minorTickVisibleProperty
+  def minorTickVisible_= (v: Boolean) {
+    minorTickVisible() = v
+  }
+  
+  def scale = delegate.scaleProperty
+  
+  def tickLabelFormatter = delegate.tickLabelFormatterProperty
+  def tickLabelFormatter_= (v: StringConverter[T]) {
+    tickLabelFormatter() = v
+  }
+  
+  def upperBound = delegate.upperBoundProperty
+  def upperBound_= (v: Double) {
+    upperBound() = v
+  }
+  
+}
