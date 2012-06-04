04030efbf400ac1c0462a5b483baee8af630fcf5
Brian Schlining <brian@mbari.org>
2012-06-03 20:57:36 -0700

    changed setNode to node

--- src/scalafx/scene/input/Mnemonic.scala
+++ src/scalafx/scene/input/Mnemonic.scala
@@ -64,6 +64,6 @@ class Mnemonic(override val delegate: jfxsi.Mnemonic)
   /**
    * Sets the Node
    */
-  def setNode_=(node: Node) { delegate.setNode(node) }
+  def node_=(n: Node) { delegate.setNode(n) }
 
 }
