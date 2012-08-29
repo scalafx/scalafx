2eadfe5d1553e7bf76658833a2b3d0611feeebbe
rafael.afonso <devnull@localhost>
2012-08-29 16:08:31 +1000

    Canvas Demos. Added CanvasTest. removedRmethods inside LayerTest. Copyright information in all 4 files.

---
+++ demo/scalafx/canvas/CanvasTest.scala
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2012, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package scalafx.canvas
+
+import scalafx.application.JFXApp
+import scalafx.scene.canvas.Canvas
+import scalafx.scene.effect.DropShadow
+import scalafx.scene.paint.CycleMethod.sfxCycleMethod2jfx
+import scalafx.scene.paint.Stop.sfxStop2jfx
+import scalafx.scene.paint.Color
+import scalafx.scene.paint.CycleMethod
+import scalafx.scene.paint.LinearGradient
+import scalafx.scene.paint.RadialGradient
+import scalafx.scene.paint.Stop
+import scalafx.scene.Scene
+import scalafx.stage.Stage
+
+/**
+ * Example adapted from code showed in [[http://docs.oracle.com/javafx/2/canvas/jfxpub-canvas.htm]].
+ */
+object CanvasTest extends JFXApp {
+
+  val canvas = new Canvas(200, 200)
+  val gc = canvas.graphicsContext2D
+
+  /*
+   * Moves the canvas to a new location within the Scene. This is accomplished
+   * by performing a translation transformation on the Canvas object, passing
+   * in the desired x and y coordinates. Passing in values of 0,0 will position
+   * the Canvas in the upper left corner of the Scene.
+   */
+  //  moveCanvas(0, 0)-
+  canvas.translateX = 0
+  canvas.translateY = 0
+
+  /*
+   * Draws an area in the shape of a capital letter "D."
+   * The user can try substituting numbers
+   * of their own in the bezierCurveTo parameters to
+   * warp the shape away from the letter "D."
+   */
+  //  drawDShape
+  gc.beginPath
+  gc.moveTo(50, 50)
+  gc.bezierCurveTo(150, 20, 150, 150, 75, 150)
+  gc.closePath
+
+  /*
+   * Draws a radial gradient on the Canvas object, which appears as a series of
+   * circles radiating outward. This demo uses RED and YELLOW by default.
+   */
+  //  drawRadialGradient(Color.RED, Color.YELLOW)
+  gc.fill = new RadialGradient(0, 0, 0.5, 0.5, 0.1, true, CycleMethod.REFLECT, List(Stop(0.0, Color.RED), Stop(1.0, Color.YELLOW)))
+  gc.fillPath
+
+  /*
+   * Draws a radial gradient on the Canvas object, which appears as a series of
+   * circles radiating outward. This demo uses RED and YELLOW by default.
+   */
+  //  drawLinearGradient(Color.BLUE, Color.GREEN)
+  val lg = new LinearGradient(0, 0, 1, 1, true, CycleMethod.REFLECT, List(Stop(0.0, Color.BLUE), Stop(1.0, Color.GREEN)))
+  gc.stroke = lg
+  gc.lineWidth = 20
+  gc.strokePath
+
+  /**
+   * Draws a radial gradient on the Canvas object, which appears as a series of
+   * circles radiating outward. This demo uses RED and YELLOW by default.
+   */
+  //  drawDropShadow(Color.GRAY, Color.BLUE, Color.GREEN, Color.RED)
+  gc.applyEffect(new DropShadow(20, 20, 0, Color.GRAY))
+  gc.applyEffect(new DropShadow(20, 0, 20, Color.BLUE))
+  gc.applyEffect(new DropShadow(20, -20, 0, Color.GREEN))
+  gc.applyEffect(new DropShadow(20, 0, -20, Color.RED))
+
+  stage = new Stage {
+    title = "Canvas Test"
+    scene = new Scene(400, 400) {
+      content = canvas
+    }
+  }
+
+}
--- demo/scalafx/canvas/LayerTest.scala
+++ demo/scalafx/canvas/LayerTest.scala
@@ -1,3 +1,29 @@
+/*
+ * Copyright (c) 2012, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package scalafx.canvas
 
 import scalafx.Includes._
@@ -6,7 +32,6 @@ import scalafx.beans.property.ObjectProperty.sfxObjectProperty2jfx
 import scalafx.collections.ObservableBuffer
 import scalafx.scene.Group.sfxGroup2jfx
 import scalafx.scene.canvas.Canvas
-import scalafx.scene.canvas.GraphicsContext
 import scalafx.scene.control.SelectionModel
 import scalafx.scene.control.ChoiceBox
 import scalafx.scene.input.MouseEvent
@@ -22,74 +47,56 @@ import scalafx.stage.Stage
  */
 object LayerTest extends JFXApp {
 
-  private var root: Group = null
-  private var borderPane: BorderPane = null
-  private var layer1: Canvas = null
-  private var layer2: Canvas = null
-  private var gc1: GraphicsContext = null
-  private var gc2: GraphicsContext = null
-  private var cb: ChoiceBox[String] = null
+  //  Create Layers
+  // Layers 1&2 are the same size
+  private val layer1 = new Canvas(300, 250)
+  private val layer2 = new Canvas(300, 250)
 
-  private def createLayers {
-    // Layers 1&2 are the same size
-    layer1 = new Canvas(300, 250)
-    layer2 = new Canvas(300, 250)
+  // Obtain Graphics Contexts
+  private val gc1 = layer1.graphicsContext2D
+  gc1.fill = Color.GREEN
+  gc1.fillOval(50, 50, 20, 20);
+  private val gc2 = layer2.graphicsContext2D
+  gc2.fill = Color.BLUE
+  gc2.fillOval(100, 100, 20, 20);
 
-    // Obtain Graphics Contexts
-    gc1 = layer1.graphicsContext2D
-    gc1.fill = Color.GREEN
-    gc1.fillOval(50, 50, 20, 20);
-    gc2 = layer2.graphicsContext2D
-    gc2.fill = Color.BLUE
-    gc2.fillOval(100, 100, 20, 20);
+  // Handle Layers
+  // Handler for Layer 1
+  layer1.onMousePressed = (e: MouseEvent) => {
+    gc1.fillOval(e.x, e.y, 20, 20)
+  }
+  // Handler for Layer 2
+  layer2.onMousePressed = (e: MouseEvent) => {
+    gc2.fillOval(e.x, e.y, 20, 20)
   }
 
-  private def handleLayers() {
-    // Handler for Layer 1
-    layer1.onMousePressed = (e: MouseEvent) => {
-      gc1.fillOval(e.x, e.y, 20, 20)
-    }
-
-    // Handler for Layer 2
-    layer2.onMousePressed = (e: MouseEvent) => {
-      gc2.fillOval(e.x, e.y, 20, 20)
-    }
+  // Create Choice Box 
+  val layer1Title = "Layer 1 is GREEN"
+  val layer2Title = "Layer 2 is BLUE"
+  private val cb = new ChoiceBox[String] {
+    items = ObservableBuffer(layer1Title, layer2Title)
   }
-
-  private def createChoiceBox {
-    val layer1Title = "Layer 1 is GREEN"
-    val layer2Title = "Layer 2 is BLUE"
-    cb = new ChoiceBox[String] {
-      items = ObservableBuffer(layer1Title, layer2Title)
+  val selectionModel: SelectionModel[String] = cb.selectionModel.get
+  selectionModel.selectedItem.onChange((ov: Any, olaValue: Any, newValue: Any) => {
+    if (newValue == layer1Title) {
+      layer1.toFront
+    } else if (newValue == layer2Title) {
+      layer2.toFront
     }
-    val selectionModel: SelectionModel[String] = cb.selectionModel.get
-    selectionModel.selectedItem.onChange((ov: Any, olaValue: Any, newValue: Any) => {
-      if(newValue == layer1Title) {
-        layer1.toFront
-      } else if(newValue == layer2Title) {
-        layer2.toFront
-      }
-    })
-    cb.value = layer1Title
-  }
-
-  private def addLayers() {
-    // Add Layers
-    borderPane.top = cb
-    borderPane.center = new Pane {
-      content = List(layer1, layer2)
-    }
-    layer1.toFront
-    root.children = borderPane
-  }
+  })
+  cb.value = layer1Title
 
   // Build GUI
-  borderPane = new BorderPane();
-  root = new Group
-  createLayers
-  handleLayers
-  createChoiceBox
-  addLayers
+  private val borderPane = new BorderPane();
+  // Add Layers
+  borderPane.top = cb
+  borderPane.center = new Pane {
+    content = List(layer1, layer2)
+  }
+  layer1.toFront
+
+  private val root = new Group
+  root.children = borderPane
 
   stage = new Stage {
     title = "Layer Test"
