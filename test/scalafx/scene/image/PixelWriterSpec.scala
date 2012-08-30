24869de92b0214d42b09bd22c81da2715703dc49
rafael.afonso <devnull@localhost>
2012-08-30 18:21:35 +1000

    image package: Spec tests. Unfortunately it was not possible create tests for PixelFormat and WritablePixelFormat because of problems with generics.

---
+++ test/scalafx/scene/image/PixelWriterSpec.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2012, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package scalafx.scene.image
+
+import java.nio.Buffer
+import java.nio.ByteBuffer
+import java.nio.IntBuffer
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import javafx.scene.{image => jfxsi}
+import javafx.scene.{paint => jfxsp}
+import scalafx.Includes._
+import scalafx.testutil.SimpleSFXDelegateSpec
+
+/**
+ * PixelWriter Spec tests.
+ *
+ */
+@RunWith(classOf[JUnitRunner])
+class PixelWriterSpec
+  extends SimpleSFXDelegateSpec[jfxsi.PixelWriter, PixelWriter](classOf[jfxsi.PixelWriter], classOf[PixelWriter]) {
+
+  override protected def getScalaClassInstance = new PixelWriter {
+    override val delegate = getJavaClassInstance
+  }
+
+  protected def convertScalaClassToJavaClass(sfxPixelWriter: PixelWriter) = {
+    val jfxPixelWriter: jfxsi.PixelWriter = sfxPixelWriter
+    jfxPixelWriter
+  }
+
+  override protected def getJavaClassInstance = new jfxsi.PixelWriter {
+  def getPixelFormat = null
+  def setArgb(x: Int, y: Int, argb: Int) {}
+  def setColor(x: Int, y: Int, c: jfxsp.Color) {}
+  def setPixels(x: Int, y: Int, w: Int, h: Int, pixelformat: jfxsi.PixelFormat[java.nio.ByteBuffer], buffer: Array[Byte], offset: Int, scanlineStride: Int) {}
+  def setPixels(x: Int, y: Int, w: Int, h: Int, pixelformat: jfxsi.PixelFormat[java.nio.IntBuffer], buffer: Array[Int], offset: Int, scanlineStride: Int) {}
+  def setPixels[B <: Buffer](x: Int, y: Int, w: Int, h: Int, pixelformat: jfxsi.PixelFormat[B], buffer: B, scanlineStride: Int) {}
+  def setPixels(dstx: Int, dsty: Int, w: Int, h: Int, reader: jfxsi.PixelReader, srcx: Int, srcy: Int) {}
+  }
+
+  protected def convertJavaClassToScalaClass(jfxPixelWriter: jfxsi.PixelWriter) = {
+    val sfxPixelWriter: PixelWriter = jfxPixelWriter
+    sfxPixelWriter
+  }
+}
---
+++ test/scalafx/scene/image/WritableImageSpec.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2012, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package scalafx.scene.image
+
+import java.nio.Buffer
+import java.nio.ByteBuffer
+import java.nio.IntBuffer
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import javafx.scene.{image => jfxsi}
+import javafx.scene.{paint => jfxsp}
+import scalafx.Includes._
+import scalafx.testutil.SimpleSFXDelegateSpec
+
+/**
+ * WritableImage Spec tests.
+ *
+ */
+@RunWith(classOf[JUnitRunner])
+class WritableImageSpec
+  extends SimpleSFXDelegateSpec[jfxsi.WritableImage, WritableImage](classOf[jfxsi.WritableImage], classOf[WritableImage]) {
+
+  override protected def getScalaClassInstance = new WritableImage(getJavaClassInstance)
+
+  protected def convertScalaClassToJavaClass(sfxWritableImage: WritableImage) = {
+    val jfxWritableImage: jfxsi.WritableImage = sfxWritableImage
+    jfxWritableImage
+  }
+
+  override protected def getJavaClassInstance = new jfxsi.WritableImage(1, 1)
+
+  protected def convertJavaClassToScalaClass(jfxWritableImage: jfxsi.WritableImage) = {
+    val sfxWritableImage: WritableImage = jfxWritableImage
+    sfxWritableImage
+  }
+}
