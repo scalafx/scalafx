9db7f25205807ea0e54a3f147908e61467be89d4
Jarek Sacha <jsacha@users.sf.net>
2012-12-29 16:39:53 -0500

    Prototype solution to Issue 14.

--- src/scalafx/beans/property/ObjectProperty.scala
+++ src/scalafx/beans/property/ObjectProperty.scala
@@ -26,7 +26,7 @@
  */
 package scalafx.beans.property
 
-import javafx.beans.{ property => jfxbp }
+import javafx.beans.{property => jfxbp}
 import scalafx.util.SFXDelegate
 
 object ObjectProperty {
@@ -34,11 +34,23 @@ object ObjectProperty {
 
   /**
    * Creates a new ObjectProperty instance using the SimpleObjectProperty as the target.
-   * 
+   *
    * @param value the initial value
    * @return      the observable instance
    */
   implicit def apply[J <: AnyRef](value: J) = new ObjectProperty[J](new jfxbp.SimpleObjectProperty[J](value))
+
+  /** Prototype solution to Issue 14 [[http://code.google.com/p/scalafx/issues/detail?id=14]].
+    *
+    * There is a problem if an object property contains a ScalaFX wrapper and one attempts to bind it to a
+    * JavaFX ObjectProperty containing a corresponding delegate.  See example of that that is provided with Issue 14.
+    *
+    * The approach here is to create jfxbp.ObjectProperty delegate that holds the delegate of the ScalaFX value
+    * rather than value itself. This enables correct binging, and current implicit conversions should handle places
+    * where the value is used as ScalaFX wrapper.
+    */
+  def create[J <: AnyRef](bean: Object, name: String, initialValue: SFXDelegate[J]) =
+    new ObjectProperty[J](new jfxbp.SimpleObjectProperty[J](bean, name, initialValue.delegate))
 }
 
 class ObjectProperty[J <: AnyRef](override val delegate: jfxbp.ObjectProperty[J] = new jfxbp.SimpleObjectProperty[J])
