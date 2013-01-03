b0881fac3612c5a3c1aff6b13ae6aba63dcb54f4
rafael.afonso <devnull@localhost>
2013-01-03 17:41:56 +1100

    Issue 19 (http://code.google.com/p/scalafx/issues/detail?id=19): Translate should be a Transform subclass.

--- src/scalafx/scene/transform/Translate.scala
+++ src/scalafx/scene/transform/Translate.scala
@@ -16,8 +16,9 @@ object Translate {
  *
  */
 class Translate(override val delegate: jfxst.Translate = new jfxst.Translate)
-  extends SFXDelegate[jfxst.Translate]
-  with PositionDelegate[jfxst.Translate] {
+  extends Transform(delegate)
+  with PositionDelegate[jfxst.Translate]
+  with SFXDelegate[jfxst.Translate] {
 
   def this(x: Double, y: Double) = this(new jfxst.Translate(x, y))
 
