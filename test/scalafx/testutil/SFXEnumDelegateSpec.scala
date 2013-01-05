5fa2d9bfef567c7d00e6bbcbeae2dc483c901a9e
rafael.afonso <devnull@localhost>
2013-01-05 16:37:13 +1100

    scalafx.animation: Added Animation.Status and PathTransition.OrientationType. SFXEnumDelegateSpec: "values" added in isSpecialMethodName.

--- test/scalafx/testutil/SFXEnumDelegateSpec.scala
+++ test/scalafx/testutil/SFXEnumDelegateSpec.scala
@@ -90,7 +90,7 @@ abstract class SFXEnumDelegateSpec[E <: java.lang.Enum[E], S <: SFXEnumDelegate[
    * companions objects. Therefore, "valueOf" is being excluded from methods search.
    */
   protected override def isSpecialMethodName(name: String) = super.isImplementation(name) ||
-    (name == "valueOf") || name.startsWith("is") || name.startsWith("get")
+    (name == "values") || (name == "valueOf") || name.startsWith("is") || name.startsWith("get")
 
   // Simply it gets the first constant available.
   override protected def getScalaClassInstance = companion.values.toList.head
