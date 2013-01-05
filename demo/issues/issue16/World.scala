f49f546b173013c0a6249971f60b2e81cafd01ca
Jarek Sacha <jsacha@users.sf.net>
2013-01-05 11:24:33 -0500

    Modify Issue 16 sample code to use new fixed binding, add more comments.

--- demo/issues/issue16/World.scala
+++ demo/issues/issue16/World.scala
@@ -3,15 +3,16 @@ package issues.issue16
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.scene.Scene
+import scalafx.scene.paint.Color
 import scalafx.scene.shape.Rectangle
 import scalafx.stage.Stage
 
-import javafx.scene.paint.Color
 
 /**
  * Example for Issue 16 [[http://code.google.com/p/scalafx/issues/detail?id=16]] provided by "Alain.Fa...@gmail.com".
  *
- * When replacing the javafx.scene.paint.Color import by its ScalaFX counterpart, the rectangle will be BLUE forever
+ * When replacing the javafx.scene.paint.Color import by its ScalaFX counterpart, the rectangle would be BLUE forever.
+ * The rectangle should be RED, but when mouse hovers above it it should change color to GREEN.
  */
 object World extends JFXApp {
   stage = new Stage {
@@ -26,7 +27,7 @@ object World extends JFXApp {
         width = 100
         height = 100
         fill = Color.BLUE
-        // Problem is with incorrect behviour of the binding here.
+        // Problem is with incorrect behaviour of the binding was here.
         fill <== when(hover) then Color.GREEN otherwise Color.RED
       }
     }
