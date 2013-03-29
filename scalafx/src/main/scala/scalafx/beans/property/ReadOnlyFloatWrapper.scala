8b02586f1cc8d8c1f096acc22c282d83f4c03921
Jarek Sacha <jpsacha@gmail.com>
2013-03-28 22:43:38 -0400

    Fix Issue 45: Missing wrappers for ReadOnly[Boolean,Double,Float,Integer,Long,String]Wrappers

---
+++ scalafx/src/main/scala/scalafx/beans/property/ReadOnlyFloatWrapper.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2012-2013, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package scalafx.beans.property
+
+import javafx.beans.{property => jfxbp}
+import scalafx.Includes._
+import scalafx.delegate.SFXDelegate
+
+
+object ReadOnlyFloatWrapper {
+  implicit def sfxReadOnlyFloatWrapper2jfx(w: ReadOnlyFloatWrapper) = w.delegate
+
+  /** Creates a new ReadOnlyFloatWrapper instance.
+    * @param value the initial value of the wrapped value
+    */
+  def apply(value: Float) = new ReadOnlyFloatWrapper(new jfxbp.ReadOnlyFloatWrapper(value))
+}
+
+
+/** Wrapper for [[javafx.beans.property.ReadOnlyFloatWrapper]] */
+class ReadOnlyFloatWrapper(override val delegate: jfxbp.ReadOnlyFloatWrapper = new jfxbp.ReadOnlyFloatWrapper())
+  extends FloatProperty(delegate)
+  with SFXDelegate[jfxbp.ReadOnlyFloatWrapper] {
+
+  /** Creates a new ReadOnlyFloatWrapper instance.
+    * @param bean the bean of this ReadOnlyFloatWrapper
+    * @param name the name of this ReadOnlyFloatWrapper
+    */
+  def this(bean: Object, name: String) = this(new jfxbp.ReadOnlyFloatWrapper(bean, name))
+
+  /** Creates a new ReadOnlyFloatWrapper instance.
+    * @param value the initial value of the wrapped value
+    * @param bean the bean of this ReadOnlyFloatWrapper
+    * @param name the name of this ReadOnlyFloatWrapper
+    */
+  def this(bean: Object, name: String, value: Float) = this(new jfxbp.ReadOnlyFloatWrapper(bean, name, value))
+
+  /** The read-only property, that is synchronized with this ReadOnlyFloatWrapper. */
+  def readOnlyProperty: ReadOnlyFloatProperty = delegate.getReadOnlyProperty
+}
---
+++ scalafx/src/main/scala/scalafx/beans/property/ReadOnlyLongWrapper.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2012-2013, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package scalafx.beans.property
+
+import javafx.beans.{property => jfxbp}
+import scalafx.Includes._
+import scalafx.delegate.SFXDelegate
+
+
+object ReadOnlyLongWrapper {
+  implicit def sfxReadOnlyLongWrapper2jfx(w: ReadOnlyLongWrapper) = w.delegate
+
+  /** Creates a new ReadOnlyLongWrapper instance.
+    * @param value the initial value of the wrapped value
+    */
+  def apply(value: Long) = new ReadOnlyLongWrapper(new jfxbp.ReadOnlyLongWrapper(value))
+}
+
+
+/** Wrapper for [[javafx.beans.property.ReadOnlyLongWrapper]] */
+class ReadOnlyLongWrapper(override val delegate: jfxbp.ReadOnlyLongWrapper = new jfxbp.ReadOnlyLongWrapper())
+  extends LongProperty(delegate)
+  with SFXDelegate[jfxbp.ReadOnlyLongWrapper] {
+
+  /** Creates a new ReadOnlyLongWrapper instance.
+    * @param bean the bean of this ReadOnlyLongWrapper
+    * @param name the name of this ReadOnlyLongWrapper
+    */
+  def this(bean: Object, name: String) = this(new jfxbp.ReadOnlyLongWrapper(bean, name))
+
+  /** Creates a new ReadOnlyLongWrapper instance.
+    * @param value the initial value of the wrapped value
+    * @param bean the bean of this ReadOnlyLongWrapper
+    * @param name the name of this ReadOnlyLongWrapper
+    */
+  def this(bean: Object, name: String, value: Long) = this(new jfxbp.ReadOnlyLongWrapper(bean, name, value))
+
+  /** The read-only property, that is synchronized with this ReadOnlyLongWrapper. */
+  def readOnlyProperty: ReadOnlyLongProperty = delegate.getReadOnlyProperty
+}
