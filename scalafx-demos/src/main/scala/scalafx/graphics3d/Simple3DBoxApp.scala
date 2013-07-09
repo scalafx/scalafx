f7a061f14cc70ac27fab5d00a859626cbfb09929
Jarek Sacha <jpsacha@gmail.com>
2013-07-08 23:03:36 -0400

    Update demo to use only SFX8 API (remove JFX8).

--- scalafx-demos/src/main/scala/scalafx/graphics3d/Simple3DBoxApp.scala
+++ scalafx-demos/src/main/scala/scalafx/graphics3d/Simple3DBoxApp.scala
@@ -1,31 +1,56 @@
+/*
+ * Copyright (c) 2011-2013, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package scalafx.graphics3d
-
-import javafx.scene.SubScene
-import javafx.scene.shape.Box
 
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.scene.{Group, Parent, Scene, PerspectiveCamera}
+import scalafx.scene._
 import scalafx.scene.transform.{Translate, Rotate}
 import scalafx.scene.paint.{Color, PhongMaterial}
-import scalafx.scene.shape.DrawMode
+import scalafx.scene.shape.{Box, DrawMode}
 
 /** Demo of a triangular frame of a 3D box based on example in Ensemble 8. */
 object Simple3DBoxApp extends JFXApp {
 
   stage = new PrimaryStage {
-    scene = new Scene(createContent) {
+    scene = new Scene {
+      content = createContent()
       resizable = false
     }
   }
 
-  def createContent: Parent = {
-    val testBox = new Box(5, 5, 5)
-    testBox.setMaterial(new PhongMaterial(Color.RED))
-    testBox.setDrawMode(DrawMode.LINE)
+  def createContent(): Node = {
+    val testBox = new Box(5, 5, 5) {
+      material = new PhongMaterial(Color.RED)
+      drawMode = DrawMode.LINE
+    }
 
-    val camera = new PerspectiveCamera(true) {
+    val perspectiveCamera = new PerspectiveCamera(true) {
       transforms +=(
         new Rotate(-20, Rotate.YAxis),
         new Rotate(-20, Rotate.XAxis),
@@ -33,15 +58,12 @@ object Simple3DBoxApp extends JFXApp {
     }
 
     val root = new Group {
-      children ++= Seq(camera, testBox)
+      children ++= Seq(perspectiveCamera, testBox)
     }
 
-    val subScene = new SubScene(root, 300, 300)
-    subScene.setFill(Color.ALICEBLUE)
-    subScene.setCamera(camera)
-
-    new Group {
-      children += subScene
+    new SubScene(root, 300, 300) {
+      fill = Color.ALICEBLUE
+      camera = perspectiveCamera
     }
   }
 }
