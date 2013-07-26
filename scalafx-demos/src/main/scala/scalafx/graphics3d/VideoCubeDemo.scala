6ff2c629266421bfb2ede60c73639fa86bf2c580
Jarek Sacha <jpsacha@gmail.com>
2013-07-26 16:28:12 -0400

    Use a system property to define location of movie samples (instead of hardcoded value).

--- scalafx-demos/src/main/scala/scalafx/graphics3d/VideoCubeDemo.scala
+++ scalafx-demos/src/main/scala/scalafx/graphics3d/VideoCubeDemo.scala
@@ -34,10 +34,10 @@ import scalafx.animation.Timeline
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.DepthTest
-import scalafx.scene.media.{Media,MediaView,MediaPlayer}
+import scalafx.scene.media.{Media, MediaView, MediaPlayer}
 import scalafx.scene.paint.Color
 import scalafx.scene.shape.Rectangle
-import scalafx.scene.text.{Text,Font}
+import scalafx.scene.text.{Text, Font}
 import scalafx.scene.transform.Rotate
 import scalafx.scene.{Node, PerspectiveCamera, Scene, Group}
 
@@ -53,7 +53,7 @@ object VideoCubeDemo extends JFXApp {
   var animation: Timeline = _
   var root = new Group
   stage = new PrimaryStage {
-    scene = new Scene( root, 800, 600, true )  {
+    scene = new Scene(root, 800, 600, true) {
       fill = Color.BLACK
     }
     resizable = false
@@ -67,14 +67,21 @@ object VideoCubeDemo extends JFXApp {
   // to any server, PUBLIC, PROTECTED and PRIVATE without the studio consent ;-))
   // PLEASE DO NOT SHOW THIS PUBLIC LIKE IN A BIG CONFERENCE with consent either
   // This is just for learning and demonstration purposes.
-  // Of course, it will only work my machine!!!!!!!!!!!!!!!!!!!!!! Until you edit the folder and files!
-  val folder = if (System.getProperty("os.name").toLowerCase.contains("windows")) {
-      new File( "C:\\Users\\peter\\Videos\\Movie-Trailers-2012" )
+
+  // Location of the movie files has to be defined using system property `scalafx.graphics3d.VideoCubeDemo.folder`
+  // For instance using command line option `-Dscalafx.graphics3d.VideoCubeDemo.folder=/tmp/videos`
+  val folderSysProperty = "scalafx.graphics3d.VideoCubeDemo.folder"
+  val folderOption = Option[String](System.getProperty(folderSysProperty))
+
+  val folder = folderOption match {
+    case Some(folderName) => {
+      val file = new File(folderName)
+      if (file.exists() && file.isDirectory) file
+      else throw new IllegalArgumentException("System property `" + folderSysProperty + " = " + folderName + "` " +
+        "has to point to an existing directory.")
     }
-    else {
-      new File( "/Users/peterpilgrim/Movies/Movie-Trailers-2012" )
-    }
-  // TODO: +PP+ Dec 2012 - This all should be defined by a preferences (property file) in user home
+    case None => throw new IllegalArgumentException("System property `" + folderSysProperty + "` is not defined.")
+  }
 
   // You need your video files ;-) Cannot redistribute MOVIE FILES!!!
   // Substitute, for example, with your own family and vacation pictures
@@ -84,13 +91,13 @@ object VideoCubeDemo extends JFXApp {
     "PROMETHEUS Trailer 2 - 2012 Movie - Official [HD].mp4",
     "RESIDENT EVIL 5 Retribution Trailer 2 - 2012 Movie - Official.mp4",
     "TED Movie Trailer 2012 - Official [HD].mp4",
-    "THE HOBBIT Trailer - 2012 Movie - Official [HD].mp4" )
+    "THE HOBBIT Trailer - 2012 Movie - Official [HD].mp4")
 
   val mediaPlayers = vidFiles.map {
-    filename =>  {
-      val file = new File( folder, filename)
-      val media = new Media( file.toURI.toURL.toExternalForm )
-      val mediaPlayer = new MediaPlayer( media )
+    filename => {
+      val file = new File(folder, filename)
+      val media = new Media(file.toURI.toURL.toExternalForm)
+      val mediaPlayer = new MediaPlayer(media)
       mediaPlayer.volume = 0.5
       mediaPlayer.cycleCount = MediaPlayer.Indefinite
       mediaPlayer
@@ -103,27 +110,30 @@ object VideoCubeDemo extends JFXApp {
 
   //  cubeSystem.getTransforms.addAll( new Translate(800 / 2, 600 / 2), new Rotate(180, Rotate.XAxis) )
 
-  stage.getScene.setCamera( new PerspectiveCamera() )
+  stage.getScene.setCamera(new PerspectiveCamera())
 
-  val lowX = -75; val lowY = -75; val highX = 900; val highY = 700
+  val lowX = -75
+  val lowY = -75
+  val highX = 900
+  val highY = 700
   val starryBackground = new Group {
-    val stars = (1 to 500).map( x => new Rectangle {
-      x =  lowX + scala.math.random * (highX - lowX )
-      y =  lowY + scala.math.random * (highY - lowY )
+    val stars = (1 to 500).map(x => new Rectangle {
+      x = lowX + scala.math.random * (highX - lowX)
+      y = lowY + scala.math.random * (highY - lowY)
       //      printf("x=%5.1f, y=%5.1f\n", x.get(), y.get() )
       val s = 1 + scala.math.random * 3
       width = s
       height = s
       fill = Color.WHITE
-    } )
+    })
     //    printf("stars=%s\n", stars)
     children = stars
     translateZ = 100.0
   }
 
-  root.children.addAll( starryBackground, cubeSystem )
+  root.children.addAll(starryBackground, cubeSystem)
 
-  def create3dContent():Node = {
+  def create3dContent(): Node = {
     val c1 = new VideoCube(mediaPlayers, 300)
     c1.rx.angle = 45
     c1.ry.angle = 45
@@ -133,11 +143,11 @@ object VideoCubeDemo extends JFXApp {
     animation = new Timeline {
       cycleCount = Timeline.INDEFINITE
       keyFrames = Seq(
-        at (0 s) { c1.ry.angle -> 0d ; c1.rx.angle -> 0d; c1.rz.angle -> 0d },
-        at (4 s) { c1.rx.angle -> 360d ; c1.ry.angle -> 520d; },
-//        at (4 s) { c1.rz.angle -> 0d ; c1.ry.angle -> 720d; }
-        at (7 s) { c1.rx.angle -> 360d ; c1.ry.angle -> 520d },
-        at (10 s) { c1.rz.angle -> 1080d }
+        at(0 s) {c1.ry.angle -> 0d; c1.rx.angle -> 0d; c1.rz.angle -> 0d},
+        at(4 s) {c1.rx.angle -> 360d; c1.ry.angle -> 520d;},
+        //        at (4 s) { c1.rz.angle -> 0d ; c1.ry.angle -> 720d; }
+        at(7 s) {c1.rx.angle -> 360d; c1.ry.angle -> 520d},
+        at(10 s) {c1.rz.angle -> 1080d}
       )
     }
 
@@ -146,73 +156,85 @@ object VideoCubeDemo extends JFXApp {
 
   def play() {
     animation.play()
-    for ( mp <- mediaPlayers ) { mp.play() }
+    for (mp <- mediaPlayers) {mp.play()}
   }
 
   def stop() {
     animation.pause()
-    for ( mp <- mediaPlayers ) { mp.stop() }
+    for (mp <- mediaPlayers) {mp.stop()}
   }
 
   play()
 }
 
 
-class VideoCube( val mediaPlayers: List[MediaPlayer], size: Double ) extends Group {
-  val rx = new Rotate(0,Rotate.XAxis)
-  val ry = new Rotate(0,Rotate.YAxis)
-  val rz = new Rotate(0,Rotate.ZAxis)
+class VideoCube(val mediaPlayers: List[MediaPlayer], size: Double) extends Group {
+  val rx = new Rotate(0, Rotate.XAxis)
+  val ry = new Rotate(0, Rotate.YAxis)
+  val rz = new Rotate(0, Rotate.ZAxis)
 
   transforms = Seq(rz, ry, rx)
 
   children = Seq(
-    new MediaViewCubeFace( mediaPlayers(0) ) { // back face
-      fitWidth = size; fitHeight = size
-      translateX = -0.5*size
-      translateY = -0.5*size
-      translateZ = 0.5*size
-//      preserveRatio = false
+    new MediaViewCubeFace(mediaPlayers(0)) {
+      // back face
+      fitWidth = size
+      fitHeight = size
+      translateX = -0.5 * size
+      translateY = -0.5 * size
+      translateZ = 0.5 * size
+      //      preserveRatio = false
       text = "0"
     },
-    new MediaViewCubeFace( mediaPlayers(1), 0.01  ) { // bottom face
-      fitWidth = size ; fitHeight = size
-      translateX = -0.5*size
+    new MediaViewCubeFace(mediaPlayers(1), 0.01) {
+      // bottom face
+      fitWidth = size
+      fitHeight = size
+      translateX = -0.5 * size
       translateY = 0
       rotationAxis = Rotate.XAxis
       rotate = 90
-//      preserveRatio = false
+      //      preserveRatio = false
       text = "1"
     },
-    new MediaViewCubeFace( mediaPlayers(2), 0.01 ) { // right face
-      fitWidth = size ; fitHeight = size
-      translateX = -1*size
-      translateY = -0.5*size
+    new MediaViewCubeFace(mediaPlayers(2), 0.01) {
+      // right face
+      fitWidth = size
+      fitHeight = size
+      translateX = -1 * size
+      translateY = -0.5 * size
       rotationAxis = Rotate.YAxis
       rotate = 90
-//      preserveRatio = false
+      //      preserveRatio = false
       text = "2"
     },
-    new MediaViewCubeFace( mediaPlayers(3) ) { // left face
-      fitWidth = size; fitHeight = size
+    new MediaViewCubeFace(mediaPlayers(3)) {
+      // left face
+      fitWidth = size
+      fitHeight = size
       translateX = 0
-      translateY = -0.5*size
+      translateY = -0.5 * size
       rotationAxis = Rotate.YAxis
       rotate = 90
       text = "3"
     },
-    new MediaViewCubeFace( mediaPlayers(4) ) { // top face
-      fitWidth = size; fitHeight = size
-      translateX = -0.5*size
-      translateY = -1*size
+    new MediaViewCubeFace(mediaPlayers(4)) {
+      // top face
+      fitWidth = size
+      fitHeight = size
+      translateX = -0.5 * size
+      translateY = -1 * size
       rotationAxis = Rotate.XAxis
       rotate = 90
       text = "4"
     },
-    new MediaViewCubeFace( mediaPlayers(5), 0.01 ) { // top face
-      fitWidth = size; fitHeight = size
-      translateX = -0.5*size
-      translateY = -0.5*size
-      translateZ = -0.5*size
+    new MediaViewCubeFace(mediaPlayers(5), 0.01) {
+      // top face
+      fitWidth = size
+      fitHeight = size
+      translateX = -0.5 * size
+      translateY = -0.5 * size
+      translateZ = -0.5 * size
       text = "5"
     }
   )
@@ -225,21 +247,21 @@ class VideoCube( val mediaPlayers: List[MediaPlayer], size: Double ) extends Gro
    * @param color the colour of the cube face
    * @param shade the derived colour
    */
-  class MediaViewCubeFace( val mediaPlayer: MediaPlayer, size: Double, offset: Double = -0.01,
-                           color: Color = Color.LIGHTBLUE, shade: Double = 1.0 ) extends Group {
+  class MediaViewCubeFace(val mediaPlayer: MediaPlayer, size: Double, offset: Double = -0.01,
+                          color: Color = Color.LIGHTBLUE, shade: Double = 1.0) extends Group {
 
-    def this( mediaPlayer: MediaPlayer, offset: Double ) = this( mediaPlayer, 0.0, offset )
+    def this(mediaPlayer: MediaPlayer, offset: Double) = this(mediaPlayer, 0.0, offset)
 
-    def this( mediaPlayer: MediaPlayer ) = this( mediaPlayer, 0.0 )
+    def this(mediaPlayer: MediaPlayer) = this(mediaPlayer, 0.0)
 
     val debugText = new Text {
-      font = new Font("Verdana", 36.0 )
+      font = new Font("Verdana", 36.0)
       fill = Color.ORANGE
       layoutX = size / 4
       layoutY = size / 4
     }
 
-    val mediaView = new MediaView( mediaPlayer )
+    val mediaView = new MediaView(mediaPlayer)
 
     val backRect = new Rectangle {
       x = 0
@@ -247,13 +269,13 @@ class VideoCube( val mediaPlayers: List[MediaPlayer], size: Double ) extends Gro
       translateZ = offset
       width = size
       height = size
-      fill = color.deriveColor(0.0, 1.0, (1 - 0.5*shade), 1.0)
+      fill = color.deriveColor(0.0, 1.0, 1 - 0.5 * shade, 1.0)
       depthTest = DepthTest.INHERIT
       // JavaFX Rendering on 3D with planar surfaces and MediaView will improve
       // opacity = 0.0
     }
 
-    children = Seq( backRect, mediaView, debugText )
+    children = Seq(backRect, mediaView, debugText)
 
     def fitHeight = mediaView.fitHeightProperty
     def fitHeight_=(v: Double) {
@@ -290,10 +312,5 @@ class VideoCube( val mediaPlayers: List[MediaPlayer], size: Double ) extends Gro
     }
 
   }
+
 }
-
-
-
-// http://code.google.com/p/scalafx/source/browse/
-
-// http://code.google.com/p/scalafx/source/browse/demo/scalafx/graphics3d/VideoCubeDemo.scala
