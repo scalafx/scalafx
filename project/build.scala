b4e879f57ac2dceefb0cd050c608217f1bb25d55
Jarek Sacha <jpsacha@gmail.com>
2013-10-21 20:19:26 -0400

    Upgrade working version number to "1.0.0-M7-SNAPSHOT".

--- project/build.scala
+++ project/build.scala
@@ -36,7 +36,7 @@ object ScalaFXBuild extends Build {
   import Dependencies._
   import Resolvers._
 
-  val scalafxVersion = "1.0.0-M6"
+  val scalafxVersion = "1.0.0-M7-SNAPSHOT"
 
   lazy val scalafxSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.scalafx",
