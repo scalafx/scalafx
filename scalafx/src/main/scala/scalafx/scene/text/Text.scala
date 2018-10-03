d66bedb0921a586c8508b988aa7dcb4ca500b5cf
Jarek Sacha <jpsacha@gmail.com>
2018-10-02 21:09:44 -0400

    Add wrappers Text for properties added in JavaFX 9.

--- scalafx/src/main/scala/scalafx/scene/text/Text.scala
+++ scalafx/src/main/scala/scalafx/scene/text/Text.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2016, ScalaFX Project
+ * Copyright (c) 2011-2018, ScalaFX Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,16 +26,17 @@
  */
 package scalafx.scene.text
 
-import javafx.geometry.VPos
-import javafx.scene.{text => jfxst}
-import javafx.{scene => jfxs}
-
-import scala.language.implicitConversions
+import javafx.scene.{paint => jfxsp, shape => jfxss, text => jfxst}
+import javafx.{geometry => jfxg, scene => jfxs}
 import scalafx.Includes._
 import scalafx.beans.property._
 import scalafx.delegate.{PositionDelegate, SFXDelegate}
+import scalafx.geometry.VPos
+import scalafx.scene.paint.Paint
 import scalafx.scene.shape.Shape
 import scalafx.scene.text.Font.sfxFont2jfx
+
+import scala.language.implicitConversions
 
 object Text {
   implicit def sfxText2jfx(v: Text): jfxs.text.Text = if (v != null) v.delegate else null
@@ -44,105 +45,158 @@ object Text {
 
 class Text(override val delegate: jfxst.Text = new jfxst.Text)
   extends Shape(delegate)
-  with PositionDelegate[jfxst.Text]
-  with SFXDelegate[jfxst.Text] {
+    with PositionDelegate[jfxst.Text]
+    with SFXDelegate[jfxst.Text] {
 
   /**
-   * Creates an instance of Text containing the given string.
-   */
+    * Creates an instance of Text containing the given string.
+    */
   def this(t: String) = this(new jfxst.Text(t))
 
   /**
-   * Creates an instance of Text on the given coordinates containing the given string.
-   */
+    * Creates an instance of Text on the given coordinates containing the given string.
+    */
   def this(x: Double, y: Double, t: String) = this(new jfxst.Text(x, y, t))
 
   /**
-   * The 'alphabetic' (or roman) baseline offset from the Text node's layoutBounds.minY location.
-   *
-   */
+    * The 'alphabetic' (or roman) baseline offset from the Text node's layoutBounds.minY location.
+    *
+    */
   // NOTE IMPLEMENTATION: Added "Property" suffix to not conflict with Node.baselineOffset() method.
   def baselineOffsetProperty: ReadOnlyDoubleProperty = delegate.baselineOffsetProperty
 
   /**
-   * Determines how the bounds of the text node are calculated.
-   */
+    * Determines how the bounds of the text node are calculated.
+    */
   def boundsType: ObjectProperty[jfxst.TextBoundsType] = delegate.boundsTypeProperty
   def boundsType_=(v: TextBoundsType) {
     boundsType() = v
   }
 
   /**
-   * Defines the font of text.
-   */
+    * Defines the font of text.
+    */
   def font: ObjectProperty[jfxst.Font] = delegate.fontProperty
   def font_=(v: Font) {
     font() = v
   }
 
   /**
-   * Specifies a requested font smoothing type : gray or LCD.
-   */
+    * Specifies a requested font smoothing type : gray or LCD.
+    */
   def fontSmoothingType: ObjectProperty[jfxst.FontSmoothingType] = delegate.fontSmoothingTypeProperty
   def fontSmoothingType_=(v: FontSmoothingType) {
     fontSmoothingType() = v
   }
 
   /**
-   * Defines the vertical space in pixel between lines.
-   */
+    * Defines the vertical space in pixel between lines.
+    */
   def lineSpacing: DoubleProperty = delegate.lineSpacingProperty
   def lineSpacing_=(v: Double) {
     lineSpacing() = v
   }
 
   /**
-   * Defines if each line of text should have a line through it.
-   */
+    * Defines if each line of text should have a line through it.
+    */
   def strikethrough: BooleanProperty = delegate.strikethroughProperty
   def strikethrough_=(v: Boolean) {
     strikethrough() = v
   }
 
   /**
-   * Defines text string that is to be displayed.
-   */
+    * Defines text string that is to be displayed.
+    */
   def text: StringProperty = delegate.textProperty
   def text_=(v: String) {
     text() = v
   }
 
   /**
-   * Defines horizontal text alignment in the bounding box.
-   */
+    * Defines horizontal text alignment in the bounding box.
+    */
   def textAlignment: ObjectProperty[jfxst.TextAlignment] = delegate.textAlignmentProperty
   def textAlignment_=(v: TextAlignment) {
     textAlignment() = v
   }
 
   /**
-   * Defines the origin of text coordinate system in local coordinates.
-   */
-  def textOrigin: ObjectProperty[VPos] = delegate.textOriginProperty
+    * Defines the origin of text coordinate system in local coordinates.
+    */
+  def textOrigin: ObjectProperty[jfxg.VPos] = delegate.textOriginProperty
   def textOrigin_=(v: VPos) {
     textOrigin() = v
   }
 
   /**
-   * Defines if each line of text should have a line below it.
-   */
+    * Defines if each line of text should have a line below it.
+    */
   def underline: BooleanProperty = delegate.underlineProperty
   def underline_=(v: Boolean) {
     underline() = v
   }
 
   /**
-   * Defines a width constraint for the text in user space coordinates, e.g. pixels, not glyph or
-   * character count.
-   */
+    * Defines a width constraint for the text in user space coordinates, e.g. pixels, not glyph or
+    * character count.
+    */
   def wrappingWidth: DoubleProperty = delegate.wrappingWidthProperty
   def wrappingWidth_=(v: Double) {
     wrappingWidth() = v
   }
 
+
+  /**
+    * Caret bias in the content. `true` means a bias towards the leading character edge.
+    * (true=leading/false=trailing)
+    */
+  def caretBias: BooleanProperty = delegate.caretBiasProperty
+  def caretBias_=(v: Boolean) {
+    caretBias() = v
+  }
+
+  /**
+    * The fill color of selected text.
+    */
+  def selectionFill: ObjectProperty[jfxsp.Paint] = delegate.selectionFillProperty
+  def selectionFill_=(v: Paint) {
+    selectionFill() = v
+  }
+
+  /**
+    * Caret index in the content. Set to `-1` to unset caret.
+    */
+  def caretPosition: IntegerProperty = delegate.caretPositionProperty
+  def caretPosition_=(v: Int) {
+    caretPosition() = v
+  }
+
+  /**
+    * Shape of caret in local coordinates.
+    */
+  def caretShape: ReadOnlyObjectProperty[Array[jfxss.PathElement]] = delegate.caretShapeProperty
+
+  /**
+    * Shape of selection in local coordinates.
+    */
+  def selectionShape: ReadOnlyObjectProperty[Array[jfxss.PathElement]] = delegate.selectionShapeProperty
+
+  /**
+    * Selection start index in the content.
+    * Set to `-1` to unset selection.
+    */
+  def selectionStart: IntegerProperty = delegate.selectionStartProperty
+  def selectionStart_=(v: Int) {
+    selectionStart() = v
+  }
+
+  /**
+    * Selection end index in the content.
+    * Set to `-1` to unset selection.
+    */
+  def selectionEnd: IntegerProperty = delegate.selectionEndProperty
+  def selectionEnd_=(v: Int) {
+    selectionEnd() = v
+  }
 }
