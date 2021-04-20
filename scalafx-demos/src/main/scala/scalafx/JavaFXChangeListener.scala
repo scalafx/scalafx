b05a0027015cff7ad7847d32b6dfc17b67c7dd91
Jarek Sacha <jpsacha@gmail.com>
2021-04-19 21:18:11 -0400

    Add workarounds to enable compilation with Scala 3 RC2
    Workarounds are marked with "TODO Scala 3"

--- scalafx-demos/src/main/scala/scalafx/JavaFXChangeListener.scala
+++ scalafx-demos/src/main/scala/scalafx/JavaFXChangeListener.scala
@@ -28,24 +28,35 @@
 package scalafx
 
 import scalafx.Includes._
-import scalafx.application.JFXApp
+import scalafx.application.JFXApp3
 import scalafx.scene.Scene
 import scalafx.scene.paint.Color
 import scalafx.scene.shape.Rectangle
 
-object JavaFXChangeListener extends JFXApp {
-  stage = new JFXApp.PrimaryStage {
-    title.value = "Hello Stage"
-    width = 600
-    height = 450
-    scene = new Scene {
-      fill = Color.LightGreen
-      content = new Rectangle {
-        x = 25
-        y = 40
-        width = 100
-        height = 100
-        fill <== when (hover) choose Color.Green otherwise Color.Red
+object JavaFXChangeListener extends JFXApp3 {
+  override def start(): Unit = {
+    stage = new JFXApp3.PrimaryStage {
+      title.value = "Hello Stage"
+      width = 600
+      height = 450
+      scene = new Scene {
+        fill = Color.LightGreen
+        content = new Rectangle {
+          x = 25
+          y = 40
+          width = 100
+          height = 100
+          // TODO Scala 3: Original line of code does not compile with Scala 3.0.0-RC2
+          // fill <== when (hover) choose Color.Green otherwise Color.Red
+          // NOTE Scala 3: variable `helper` was added to force type (and implicint conversions) on right side of `<==`
+          //               This is not needed in Scala 2
+
+          import javafx.scene.{paint => jfxsp}
+          import scalafx.beans.binding.ObjectBinding
+
+          val helper: ObjectBinding[jfxsp.Color] = when(hover) choose Color.Green otherwise Color.Red
+          fill <== helper
+        }
       }
     }
   }
