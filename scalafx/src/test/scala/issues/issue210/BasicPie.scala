645f42469d300e234e8305dab8b5732a94fd7ecb
Jarek Sacha <jpsacha@gmail.com>
2022-03-22 20:39:03 -0400

    Add working example from issue #210

---
+++ scalafx/src/test/scala/issues/issue210/BasicPie.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2011-2022, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package issues.issue210
+
+import scalafx.Includes.*
+import scalafx.application.JFXApp3
+import scalafx.scene.chart.PieChart
+import scalafx.scene.control.Tooltip
+import scalafx.scene.{Node, Scene}
+
+/**
+ * Corrected example from Issue 210
+ */
+object BasicPie extends JFXApp3 {
+
+  override def start(): Unit = {
+
+    val pie = new PieChart {
+      data = Seq(
+        PieChart.Data("Sun", 20),
+        PieChart.Data("IBM", 12),
+        PieChart.Data("HP", 25),
+        PieChart.Data("Dell", 22),
+        PieChart.Data("Apple", 30)
+      )
+      clockwise = false
+    }
+
+    val total = pie.data().foldLeft(0.0) { (x, y) => x + y.pieValue() }
+
+    pie.data().foreach { d =>
+      val sliceNode: Node  = d.node()
+      val pieValue: Double = d.pieValue()
+      val percent          = (pieValue / total) * 100
+      val msg              = f"${d.name()}: $pieValue%.2f ($percent%.2f%%)"
+      val tt = new Tooltip {
+        text = msg
+        style = "-fx-background-color: yellow; -fx-text-fill: black; "
+      }
+      Tooltip.install(sliceNode, tt)
+    }
+
+    stage = new JFXApp3.PrimaryStage {
+      title = "Pie Chart Example"
+      scene = new Scene {
+        root = pie
+      }
+    }
+  }
+}
