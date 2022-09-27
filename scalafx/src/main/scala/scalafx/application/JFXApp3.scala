26c0e5cc5d8588937dc5df8c415482d3588c3431
Jarek Sacha <jpsacha@gmail.com>
2022-09-27 19:24:47 -0400

    Update demo in scaladocs

--- scalafx/src/main/scala/scalafx/application/JFXApp3.scala
+++ scalafx/src/main/scala/scalafx/application/JFXApp3.scala
@@ -228,18 +228,30 @@ object JFXApp3 {
  * is called. Here is an example use:
  *
  * {{{
- *   object SimpleScalaFXApp extends JFXApp3 {
- *     override def start() : Unit = {
+ *   import scalafx.application.JFXApp3
+ *   import scalafx.application.JFXApp3.PrimaryStage
+ *   import scalafx.geometry.Insets
+ *   import scalafx.scene.Scene
+ *   import scalafx.scene.layout.HBox
+ *   import scalafx.scene.paint.Color._
+ *   import scalafx.scene.paint._
+ *   import scalafx.scene.text.Text
+ *
+ *   object JFXApp3Demo extends JFXApp3 {
+ *     override def start(): Unit = {
  *       stage = new PrimaryStage {
- *         title = "Simple ScalaFX App"
+ *         title = "ScalaFX Hello World!"
  *         scene = new Scene {
- *           root = new StackPane {
- *             padding = Insets(20)
- *             content = new Rectangle {
- *               width = 200
- *               height = 200
- *               fill = Color.DEEPSKYBLUE
- *             }
+ *           fill = Color.rgb(38, 38, 38)
+ *           content = new HBox {
+ *             padding = Insets(50, 80, 50, 80)
+ *             children = Seq(
+ *               new Text {
+ *                 text = "Hello World!"
+ *                 style = "-fx-font: normal bold 100pt sans-serif"
+ *                 fill = new LinearGradient(endX = 0, stops = Stops(Red, DarkRed))
+ *               }
+ *             )
  *           }
  *         }
  *       }
