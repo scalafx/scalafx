cf25e2120b80025673bf6003a752db81917a1661
Jarek Sacha <jpsacha@gmail.com>
2026-02-11 18:27:47 -0500

    JavaFX 25: add wrapper for HitInfo #441

---
+++ scalafx/src/main/scala/scalafx/scene/text/HitInfo.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2011-2026, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package scalafx.scene.text
+import javafx.scene.text as jfxst
+import scalafx.delegate.SFXDelegate
+
+import scala.language.implicitConversions
+
+object HitInfo {
+  implicit def sfxHitInfo2jfx(v: HitInfo): jfxst.HitInfo = if (v != null) v.delegate else null
+}
+
+/**
+ * Represents the hit information in a Text node.
+ * Wraps [[https://openjfx.io/javadoc/25/javafx.graphics/javafx/scene/text/HitInfo.html]]
+ */
+class HitInfo(override val delegate: jfxst.HitInfo)
+    extends SFXDelegate[jfxst.HitInfo] {
+
+  /**
+   * The index of the character which this hit information refers to.
+   *
+   * @return the index of the character which this hit information refers to
+   */
+  def charIndex: Int = delegate.getCharIndex
+
+  /**
+   * Indicates whether the hit is on the leading edge of the character.
+   * If it is false, it represents the trailing edge.
+   *
+   * @return if true the hit is on the leading edge of the character, otherwise
+   *         the trailing edge
+   */
+  def leading: Boolean = delegate.isLeading
+
+  /**
+   * Returns the index of the insertion position.
+   *
+   * @return the index of the insertion position
+   */
+  def insertionIndex: Int = delegate.getInsertionIndex
+}
