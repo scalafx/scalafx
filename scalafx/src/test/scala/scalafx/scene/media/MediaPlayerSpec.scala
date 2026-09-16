9cd51d02b1082ebe9063b9e9c177cb2e6836b76a
Jarek Sacha <jpsacha@gmail.com>
2026-09-16 15:48:01 -0400

    Correct regression in PR #453: fix broken media/web tests

--- scalafx/src/test/scala/scalafx/scene/media/MediaPlayerSpec.scala
+++ scalafx/src/test/scala/scalafx/scene/media/MediaPlayerSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2025, ScalaFX Project
+ * Copyright (c) 2011-2026, ScalaFX Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,7 +27,7 @@
 package scalafx.scene.media
 
 import javafx.scene.media as jfxsm
-import scalafx.CoreIncludes.*
+import scalafx.scene.media.MediaIncludes.*
 import scalafx.testutil.SimpleSFXDelegateSpec
 
 /**
